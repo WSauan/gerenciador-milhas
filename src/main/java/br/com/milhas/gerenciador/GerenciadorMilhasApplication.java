812ed3d28ec9d831a8b4fdbcb13518ab098cd109
Welber <Welber.sauan123@gmail.com>
2025-11-16 20:21:59 -0300

    atualizacao

--- src/main/java/br/com/milhas/gerenciador/GerenciadorMilhasApplication.java
+++ src/main/java/br/com/milhas/gerenciador/GerenciadorMilhasApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GerenciadorMilhasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorMilhasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorMilhasApplication.class, args);
+    }
 
 }
